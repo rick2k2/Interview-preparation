b3adaeb591adf2af4326a2be3c2e5b38cd334e40
Rick saha <68051340+rick2k2@users.noreply.github.com>
2022-04-05 17:07:16 +0530

    ans added
    This is interview prep ans

---
+++ interviewprepans.py
@@ -0,0 +1,182 @@
+Tell me about yourself?
+This answer seems weak, because of missing details
+Internships
+Work Experience
+Speaking time 2 min 11 sec
+
+Download
+Print
+Sir/Ma’am,
+
+My name is Rick Saha. I was born and brought up in Makhaltore and I am presently based out of same location.
+
+My highest degree is a full time BE / B.Tech in Computer Science from Saroj Mohan Institute of Technology, Hoogly that will complete in Aug-2023. My GPA out of 10 is 9.75.
+
+My Computer Science degree(s) is giving a range of skills that I feel are crucial for my career and life. Despite limited exposure, I have understood that hard work and determination can make anything possible. I am ready to do that. Also, I am trying to develop a habit of reading current trends and industry news.
+
+I worked on Quiz or Exam management system and Railway Reservation System . I would like to add that I am certified in 12 Month(s) Introdu... Read more
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+Why should we hire you?
+This answer seems weak, because of missing details
+Internships
+Work Experience
+Speaking time 1 min 48 sec
+
+Download
+Print
+Sir/Ma’am,
+
+Having graduated from Saroj Mohan Institute of Technology, Hoogly, which is one of the reputed colleges in the country, has exposed me to interaction and teamwork with students with high intellect and belonging to various cultures and backgrounds. Also, studying in this college gave me immense opportunities to delve in various extracurricular activities. I believe that apart from a good academic background, these qualities go a long way in shaping personalities and creating efficient workforce.
+
+My experience while working on academic projects like Quiz or Exam management system and Railway Reservation System has enabled me to think out of the box owing to the creative levels of imagination. Right from choosing a topic from an ... Read more
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+What are your short term and long term goals?
+This answer seems weak, because of missing details
+Work Experience
+Speaking time 41 sec
+
+Download
+Print
+My short term goals are to simply break into the Computer Science field. As a college graduate, I need to start building a strong presence in the industry, working for a company I respect and doing a job that I enjoy. I am really excited to apply the learnings of my degree - BE / B.Tech to solve real world problems.
+
+My long term goals are to earn new responsibilities within the company, ultimately reaching higher positions as they open and helping the company succeed in the long term.
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+Tell us more about your projects and internship?
+This answer seems weak, because of missing details
+Internships
+Speaking time 1 min 12 sec
+
+Download
+Print
+Sir/Ma’am,
+
+I have always believed that internships are a great way to apply the knowledge from the classroom to real-world experience. It not only boost our confidence but also help in developing the soft skills that are much needed at workplace and in leadership positions.
+
+Unfortunately, I haven’t really got an opportunity to intern with a company till now.
+
+As far as academic projects are concerned, I have experienced that project work enables students to think out of the box owing to the creative levels of imagination. Right from choosing a topic from an area of interest, project work involves finding creative solutions to various project-related problems and many technical challenges.
+
+Coming to my project work(s). I worked on many in... Read more
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+Tell us about your past work experience?
+This answer seems weak, because of missing details
+Internships
+Work Experience
+Speaking time 59 sec
+
+Download
+Print
+Sir/Ma’am,
+
+Being a fresher, I don’t have any full time job experience as of now..
+
+I would like to mention about my project work(s). I worked on many interesting topic(s) like Quiz or Exam management system and Railway Reservation System .
+
+These exposures and opportunities helped me pick up a lot of necessary skills that I feel are crucial for my career in this industry.
+
+To add further, I’m humble, hard-working, and very adaptable as an individual. As you can see from my resume, I’ve had the opportunity to work on project(s) in completely new working environments. I managed to complete all of them successfully and gained expertise.
+
+Also, I am not afraid of change as I treat is simply as a challenge, which is unavoidable for both peopl... Read more
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+What are your biggest achievements till date?
+This answer seems weak, because of missing details
+Internships
+Work Experience
+Speaking time 1 min 33 sec
+
+Download
+Print
+When it comes to my achievements, I am proud of many things that I have accomplished in the past few years.
+
+My biggest academic achievement will be completing my BE / B.Tech degree with a 9.75 CGPA. I would also like to mention that I secured 63.2% in my XIIth board and 72.8% in my Xth board, which I feel are also are academic achievements.
+
+Additionally, I am a WBJEE Rank 4693 holder. I can read, write and speak in Bengali/Bangla .
+
+Also, in the professional achievement sphere, I contributed substantially to many topic(s) my project work(s) like Quiz or Exam management system and Railway Reservation System .
+
+Sir/Ma’am, I am certified in 12 Month(s) Introduction to HTML and 1 Month(s) Programming Fundamental with C language, which I feel ... Read more
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+Where do you see yourself in 5 years?
+This answer seems weak, because of missing details
+Internships
+Work Experience
+Speaking time 44 sec
+
+Download
+Print
+I am truly excited about this job opening.
+
+In the next five years, I want to reach a position synonymous to team lead making significant contributions to the business bottom line.
+
+This opening will surely offer a golden opportunity for me to ad wings to my flight.
+
+With some good academic project in my kitty already, I feel I am ready to take up the responsibilities highlighted in this position.
+
+Besides, my intrinsic potential to work in teams and collaborate with people will help me bloom into a competent professional in very little time and with minimal guidance.
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+What are your strengths & weakness?
+This answer seems weak, because of missing details
+Work Experience
+Speaking time 56 sec
+
+Download
+Print
+I enjoy working in a team or a group and I feel my collaborative nature is my biggest strength. During every single academic project, I worked side-by-side with every member involved to achieve the target. By nature, I am an empathetic person and I can relate to people and understand their needs easily. Also, I work well under pressure and I have never missed a deadline in my academic years.
+
+My weakness is that I can be overly critical of myself. On completing a project, I always have a feeling that it could have been done in a better way. This happens even after I receive a positive response which leads me to overwork and often I am left feeling burned out.
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+Are you willing to relocate or travel?
+Speaking time 24 sec
+
+Download
+Print
+My preferred work location is Hyderabad / Secunderabad, Noida, Kolkata, Bengaluru / Bangalore and Delhi. However, I am absolutely willing to relocate and travel if needed for the right opportunity. And I believe that this role and company is one such good opportunity for my career growth.
+
+Also, I can start working Immediately.
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
+
+Do you have any questions for me?
+Speaking time 30 sec
+
+Download
+Print
+Yes Sir/Ma’am,
+
+1. Can you throw some light on the day-to-day responsibilities of this job ?
+
+2. What important qualities are you looking in a person who you would want to excel in this role ?
+
+3. What are your expectations from this role in the first 90 days ?
+
+4. How would you describe the company’s culture ?
+
+5. Who do you consider your top competitor and why ?
+Did you like this answer?YesNo
+
+Answer 1Answer 2Answer 3
